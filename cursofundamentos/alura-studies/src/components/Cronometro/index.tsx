a029abbb9d50b6cbdd2fa364cbbc43121ea72017
Wesley Roberto <suporte@ubbonline.org.br>
2022-03-28 17:34:15 -0300

    aula 07.02

--- cursofundamentos/alura-studies/src/components/Cronometro/index.tsx
+++ cursofundamentos/alura-studies/src/components/Cronometro/index.tsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 
-export default function Cronometro(){
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado } : Props){
     console.log('Conversao: '+ tempoParaSegundos('01:01:01'));
+
+    const [tempo, setTempo] = useState<number>(tempoParaSegundos(String(selecionado?.tempo)));
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    }, [selecionado]);
+
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            Tempo: {tempo}
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
