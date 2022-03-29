62e94d5cebeb576e012da269b3c877ec46f9e861
GeekWx <geekwx@gmail.com>
2022-03-29 05:38:37 -0300

    aula 07.07

--- cursofundamentos/alura-studies/src/components/Cronometro/index.tsx
+++ cursofundamentos/alura-studies/src/components/Cronometro/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export default function Cronometro({ selecionado } : Props){
-    console.log('Conversao: '+ tempoParaSegundos('01:01:01'));
+    
 
     const [tempo, setTempo] = useState<number>(tempoParaSegundos(String(selecionado?.tempo)));
 
@@ -22,15 +22,26 @@ export default function Cronometro({ selecionado } : Props){
     }, [selecionado]);
 
 
+    function regressiva(contador: number = 0){
+        setTimeout(() => {
+            if(contador > 0){
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            
+        }, 1000);
+    }
+
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
+    
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
 
-            <Botao>Começar!</Botao>
+            <Botao onClick={() => regressiva(tempo)}>Começar!</Botao>
         </div>
     );
 }
