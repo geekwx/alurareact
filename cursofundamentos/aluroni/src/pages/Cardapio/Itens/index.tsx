81110eab0f293e9f9408543c70e5a666e68c1821
Wesley Roberto <suporte@ubbonline.org.br>
2022-04-19 10:51:21 -0300

    aula 05.03

--- cursofundamentos/aluroni/src/pages/Cardapio/Itens/index.tsx
+++ cursofundamentos/aluroni/src/pages/Cardapio/Itens/index.tsx
@@ -5,7 +5,9 @@ import styles from './Itens.module.scss';
 export default function Itens(){
     return(
         <div className={styles.itens}>
-            {cardapio.map(item => (<Item  key={item.id}/>))}
+            {cardapio.map(item => (<Item  key={item.id}
+                {...item}
+            />))}
         </div>
     )
 }
